b7b9762b40749beef1420e9a7357dc481ac3c830
Zoumeye TOURE <ztoure@amexio.fr>
2025-05-30 19:42:17 +0200

    feat: ajout de Google Analytics pour le tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics />
         <Header />
         {children}
         <Footer />
---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,21 @@
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3MXLS9EXBK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3MXLS9EXBK');
+        `}
+      </Script>
+    </>
+  )
+} 
